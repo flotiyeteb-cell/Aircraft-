d3f6bba810e4d69084c4e82169b5fdd50178da6d
flotiyeteb-cell <flotiyeteb@gmail.com>
2026-04-26 19:03:31 +0200

    Create CrashHandler.kt

---
+++ app/src/main/kotlin/com/aircraftwar/CrashHandler.kt
@@ -0,0 +1,24 @@
+import android.content.Context
+import java.io.File
+import java.io.PrintWriter
+import java.io.StringWriter
+
+class CrashHandler(private val context: Context) : Thread.UncaughtExceptionHandler {
+
+    override fun uncaughtException(thread: Thread, throwable: Throwable) {
+        try {
+            val sw = StringWriter()
+            throwable.printStackTrace(PrintWriter(sw))
+            val errorLog = sw.toString()
+
+            val file = File(context.filesDir, "crash_log.txt")
+            file.appendText("\n\n=== CRASH ===\n$errorLog")
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        android.os.Process.killProcess(android.os.Process.myPid())
+        System.exit(1)
+    }
+}
